fb124a4bae82e55f34e826233850abee43cc8f75
josephius x <josephiusx@gmail.com>
2022-10-22 13:19:13 -0700

    31. refactoring

---
+++ routes/authRoutes.js
@@ -0,0 +1,18 @@
+const passport = require('passport');
+
+// we have to export thease routs in an arrow function to index.js so that we have access to app
+// We make sure to add app as a parameter.
+// Then we require it in index.js
+module.exports = app => {
+  app.get(
+   '/auth/google', 
+   passport.authenticate('google',{
+     scope: ['profile', 'email']
+   })
+  ),
+  
+  app.get(
+    '/auth/google/callback', 
+    passport.authenticate('google')
+  )
+}
