aff809034cfbc3df11d652312a613ec390f0a4fa
josephius x <josephiusx@gmail.com>
2022-10-23 10:31:54 -0700

    45. Deserialize User

--- services/passport.js
+++ services/passport.js
@@ -9,6 +9,13 @@ passport.serializeUser((user, done) => {
   done(null, user.id); // ID assigned to this record by mongo, not the google ID, 
 });
 
+passport.deserializeUser((id, done ) => {
+  User.findById(id)
+    .then( user => {
+      done(null, user)
+    })
+});
+
 passport.use(
   new GoogleStrategy(
     {
